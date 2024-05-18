23fbd65d2fb139164b1bb78458b425b2b9f77bd7
Atish Jain <atish_jain@yahoo.com>
2024-05-18 10:46:11 +0530

    Add files via upload
    18-May-24 Commit

---
+++ DatatypeDemo.java
@@ -0,0 +1,24 @@
+class DatatypeDemo 
+{
+	public static void main(String[] args) 
+	{
+		int rollno=101;
+		String name="Raj";
+		float height=6.5F;
+		double marks=99.89;
+		short id=111;
+		byte age=22;
+		long mno=9989241515L;
+		boolean isFeesPaid=true;
+
+		System.out.println("Rollno:"+rollno);
+		System.out.println("Name:"+name);
+		System.out.println("Height:"+height);
+		System.out.println("id:"+id);
+		System.out.println("Age:"+age);
+		System.out.println("Mobile no:"+mno);
+		System.out.println("isFeesPaid:"+isFeesPaid);
+
+
+	}
+}
