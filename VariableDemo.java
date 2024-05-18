2f72dcc9e937633ca5e823fa49146c0a2fb2364b
Atish Jain <atish_jain@yahoo.com>
2024-05-18 10:58:22 +0530

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,9 @@
+class VariableDemo 
+{
+	public static void main(String[] args) 
+	{
+		//variable without inititalization are not allowed in java, hence this code will throw compilation error
+		int age;
+		System.out.println(age);
+	}
+}
