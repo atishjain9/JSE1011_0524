68e428c51cd15774a34f4666890b2c3638b01089
Atish Jain <atish_jain@yahoo.com>
2024-05-18 10:57:27 +0530

    Add files via upload

---
+++ VarDemo.java
@@ -0,0 +1,11 @@
+class VarDemo 
+{
+	public static void main(String[] args) 
+	{
+   	   var no=100;
+       var str="Hello";
+       System.out.println(no);
+       System.out.println(str);
+
+	}
+}
---
+++ Welcome.java
@@ -0,0 +1,10 @@
+class Welcome 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This Code will Run without compilation");
+	}
+}
+
+// To run without Compilation use the below command
+// java Welcome.java 
